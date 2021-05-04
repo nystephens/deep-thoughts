917e44d8c20b95a6cb3fcb4abd6220b5715ea014
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-05-03 19:15:09 -0700

    before class. need to figure out if new mutations work, but cant figure out how to log in.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,21 @@
 const { User, Thought } = require('../models');
+const { AuthenicationError, AuthenticationError } = require('apollo-server-express');
+const { assertEnumType } = require('graphql');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: AudioContext.user._id })
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('freinds');
+
+      return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
     users: async () => {
       return User.find()
         .select('-__v -password')
@@ -20,6 +34,46 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user};
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+      
+        return thought;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
     }
   }
 };
---
+++ server/utils/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+    signtoken: function({ username, email, _id }) {
+        const payLoad = { username, email, _id };
+
+        return jwt.sign({ data: payLoad }, secret, { expiresIn: expiration });
+    },
+
+    authMiddleware: function({ req }) {
+        // allows token to be sent via req.body, re.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // separate "Bearer" from "<token value>"
+        if (req,headers.authorization) {
+            token = token
+                .split(' ')
+                .pop()
+                .trim();
+        }
+
+        // if no token, return the request object as is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // decode annd attach user data to request object
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        // return updated response
+        return req;
+    }
+
+};
