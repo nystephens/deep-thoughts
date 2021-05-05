4d3371e61a770fd629e2d94ce351348b7de2bd7b
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-05-05 10:22:36 -0700

    server commands added for production and development, router used to load components in App.js, single thought page updated.  Says QUERY_THOUGHT is not exported

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
     if (!thoughts.length) {
@@ -11,19 +12,26 @@ const ThoughtList = ({ thoughts, title }) => {
             {thoughts &&
                 thoughts.map(thought =>
                     <div key={thought._id} className="card mb-3">
-                        <p className ="card-header">
-                            {thought.username}
+                        <p className="card-header">
+                            <Link
+                                to={`/profile/${thought.username}`}
+                                style={{ fontWeight: 700 }}
+                                className="text-light"
+                            >
+                                {thought.username}
+                            </Link>{' '}
                             thought on {thought.createdAt}
                         </p>
                         <div className="card-body">
-                            <p>{thought.text}</p>
+                            <Link to={`/thought/${thought._id}`}></Link>
+                            <p>{thought.thoughtText}</p>
                             <p className="mb-0">
                                 Reactions: {thought.reactionCount} || Click to{' '}
-                                { thought.reactionCount ? 'see' : 'start' } the discussion!
+                                {thought.reactionCount ? 'see' : 'start'} the discussion!
                             </p>
                         </div>
                     </div>
-                    )
+                )
             }
         </div>
     );
---
+++ server/utils/queries.js
@@ -0,0 +1,17 @@
+export const QUERY_THOUGHT = gql`
+    query thought($id: ID!) {
+        thought(_id: $id) {
+            _id
+            thoughtText
+            createdAt
+            username
+            reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+        }
+    }
+`;
